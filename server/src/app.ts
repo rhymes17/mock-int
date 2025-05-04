98e17834d179840008b000f6971bca248db5d552
rhymes17 <devrahul1709@gmail.com>
2025-05-05 01:08:07 +0530

    chore: update app.ts to include user and skill routes

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,23 +1,29 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import errorHandler from "./middlewares/errorMiddleware";
 
 //routes imports
 import authRoutes from "./routes/auth.routes";
+import userRoutes from "./routes/user.routes";
+import skillRoutes from "./routes/skill.routes";
 
-const app = express()
+const app = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
-app.use(express.urlencoded({extended: true}))
+    credentials: true,
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.static("public"));
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/skills", skillRoutes);
 app.use(errorHandler);
 
-export default app
+export default app;
